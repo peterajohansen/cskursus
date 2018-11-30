e93aa017b3893ef1ddbb3ca29cbbb0f80e35a8f9
peterajohansen <45283120+peterajohansen@users.noreply.github.com>
2018-11-30 16:15:57 +0100

    Add files via upload

---
+++ FlowForIf/Program.cs
@@ -0,0 +1,86 @@
+using System;
+
+namespace FlowForIf
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            int j = 0;
+
+            while (i < 10)
+            {
+                i++;
+                while (j < 10)
+                {
+                    j++;
+                    string Tal = (i * j).ToString();
+                    if (( i * j ) > 50)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(Tal.PadLeft(4));
+
+                }
+                j = 0;
+                Console.WriteLine(" ");
+            }
+
+
+            for (int xx = 1; xx < 11; xx++)
+
+            {
+
+                for (int yy = 1; yy < 11; yy++)
+
+                {
+
+                    string tal = ( xx * yy ).ToString();
+
+                    if (( xx * yy ) > 50)
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    else
+
+                        Console.ForegroundColor = ConsoleColor.White;
+
+                    Console.Write(tal.PadLeft(4));
+
+                }
+
+                Console.WriteLine();
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+            }
+
+
+
+            // Hold console åben ved debug
+
+            if (System.Diagnostics.Debugger.IsAttached)
+
+            {
+
+                Console.Write("Press any key to continue . . . ");
+
+                Console.ReadKey();
+
+            }
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+
+                Console.WriteLine("Press any key to continue . . . ");
+
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
